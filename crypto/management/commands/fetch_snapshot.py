aad27ce21f4b41695f4dafaf89712e5acdf92b5e
ZarrkanGit <zarrkan@mail.ru>
2026-05-02 13:36:06 +0300

    feat: add Django project with PostgreSQL, models, admin, and fetch_snapshot command

---
+++ crypto/management/commands/fetch_snapshot.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from providers.coingecko import CoinGeckoProvider
+from crypto.models import Snapshot, CoinPrice
+
+
+class Command(BaseCommand):
+    help = 'Загружает снимок рынка с CoinGecko и сохраняет в БД'
+    # Что делает — получает данные из API, создаёт снимок, привязывает цены
+    def handle(self, *args, **options):
+        self.stdout.write('Загружаю данные с CoinGecko...')
+
+        provider = CoinGeckoProvider(per_page=10)
+        assets = provider.get_assets()
+
+        snapshot = Snapshot.objects.create() # создаю запись в БД через Django ORM
+
+        for asset in assets:
+            CoinPrice.objects.create( # создаем цену, привязывая к снимку
+                snapshot=snapshot,
+                name=asset.name,
+                symbol=asset.symbol,
+                price=asset.price,
+                change_24h=asset.change_24h,
+            )
+
+        self.stdout.write(self.style.SUCCESS(f'Снимок #{snapshot.id} создан! Сохранено {len(assets)} монет.'))
