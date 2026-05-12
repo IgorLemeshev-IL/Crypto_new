fbfaf5db036ff428d7386fc9968223aa07aeacd1
ZarrkanGit <zarrkan@mail.ru>
2026-05-12 13:47:26 +0300

    feat: add django-debug-toolbar

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/', include('crypto.api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('__debug__/', include('debug_toolbar.urls')), 
 ]
 
 urlpatterns.extend(debug_toolbar_urls())
