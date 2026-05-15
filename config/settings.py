59fa51fb63df1b118ce3c7f4b8d5034bf8dbc1bd
ZarrkanGit <zarrkan@mail.ru>
2026-05-15 18:35:28 +0300

    fix: add ALLOWED_HOSTS for production

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-m5attkh$wblhafk6)v%d*w29_9l-z@)*(vu#sgsg2eh!__@gp)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INTERNAL_IPS = [
     "0.0.0.0",
