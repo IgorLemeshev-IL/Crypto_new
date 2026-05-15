bd2610b68a066245b108e6e478b78a2b8769fc80
ZarrkanGit <zarrkan@mail.ru>
2026-05-15 16:35:47 +0300

    feat: add custom exception handler for uniform errors

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,7 @@ REST_FRAMEWORK = {
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.URLPathVersioning",
     "DEFAULT_VERSION": "v1",
     "ALLOWED_VERSIONS": ["v1"],
+    "EXCEPTION_HANDLER": "crypto.exceptions.custom_exception_handler",
 }
 
 SIMPLE_JWT = {
