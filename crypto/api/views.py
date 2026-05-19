a5d670a62ba01dea04cf8f2e3fee7a1b2c1f4cf1
ZarrkanGit <zarrkan@mail.ru>
2026-05-19 16:16:11 +0300

    feat(cache): complete Redis caching implementation

--- crypto/api/views.py
+++ crypto/api/views.py
@@ -1,5 +1,7 @@
 from celery.result import AsyncResult
 from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
@@ -23,6 +25,10 @@ class SnapshotViewSet(ReadOnlyModelViewSet):
     queryset = Snapshot.objects.prefetch_related("prices").all()
     serializer_class = SnapshotSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+    @method_decorator(cache_page(60 * 60))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
 
 class CoinPriceViewSet(ReadOnlyModelViewSet):
