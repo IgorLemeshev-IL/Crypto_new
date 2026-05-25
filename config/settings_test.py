cf0bc105edf57982e7b572eab33d9a3668d2e83a
ZarrkanGit <zarrkan@mail.ru>
2026-05-25 13:18:41 +0300

    fix(ci): add noqa to bypass ruff import star error

---
+++ config/settings_test.py
@@ -0,0 +1,15 @@
+from config.settings import *  # noqa: F403, F401
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
