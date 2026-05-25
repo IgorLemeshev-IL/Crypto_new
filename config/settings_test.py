2005b7f31206a3e99b1f9969045e2a73757ea477
ZarrkanGit <zarrkan@mail.ru>
2026-05-25 13:52:26 +0300

    fix(ci): use localhost for Redis and enable eager Celery

--- config/settings_test.py
+++ config/settings_test.py
@@ -13,3 +13,15 @@ DATABASES = {
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
+CELERY_ALWAYS_EAGER = True
+CELERY_TASK_ALWAYS_EAGER = True
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
